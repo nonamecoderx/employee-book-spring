f90b74ab50094b6af8b773ffcbb60a4429fd1b69
Иван <ivankiseliov@rambler.ru>
2022-11-15 00:25:01 +0300

    spring boot

---
+++ main/java/com/skypro/employee/service/EmployeeService.java
@@ -0,0 +1,59 @@
+//создали сервис, служит для работы с базой сотрудников.
+package com.skypro.employee.service;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("Employee firstname or lastname should be set");
+        }
+        Employee employee = new Employee(employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getDepartment(),
+                employeeRequest.getSalary());
+        this.employees.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values().stream()
+                .mapToInt(e -> e.getSalary())
+                .sum();
+    }
+
+    public Employee getSalaryMax() {
+        return employees.values()
+                .stream()
+                .max(Comparator.comparingInt(Employee::getSalary)).get();
+    }
+
+    public Employee getSalaryMin() {
+        return employees.values()
+                .stream()
+                .min(Comparator.comparingInt(Employee::getSalary)).get();
+    }
+
+    public Set<Employee> getHigherAverage() {
+        if (employees.size() == 0){
+            throw new RuntimeException("Добавьте хоть одного сотрудника");
+        }
+        int average = getSalarySum()/employees.size();
+        return employees.values().stream().filter(s->s.getSalary()>average).collect(Collectors.toSet());
+    }
+}
+
+
+
